d33f428057d6b25d473155e339c90e198599ea25
CLAUDIA <claudia_4bcn@hotmail.com>
2025-11-25 13:03:50 +0100

    Adding Hero image and blue box

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -6,15 +6,29 @@ export const Hero = () => {
       <div className="hero-container">
         
         <div className="hero-text">
+            <h1>A Simple Bookmark Manager</h1>
+            <p>
+              A clean and simple interface to organize your favourite websites.
+              Open a new browser tab and see your sites load instantly. Try it
+              for free.
+            </p>
+        <div class="hero-buttons">
         <Button variant="blue">Get it on Chrome</Button>
         <Button variant="light">Get it on Firefox</Button>
         </div>
+        </div>
 
-        <div className="hero-image">
+        <div class="hero-image">
+        <div class="blue-box"></div>
+        <img src="../assets/images/illustration-hero.svg"
+              alt="Hero image"
+              class="hero-img"
+            />
+          </div>
          
         </div>
 
-      </div>
-    </section>
+          </section>
   );
 };
+
