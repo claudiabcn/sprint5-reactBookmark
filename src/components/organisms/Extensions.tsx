54b3100849a4b068fe225301609766fa9e7f0009
CLAUDIA <claudia_4bcn@hotmail.com>
2025-11-26 12:11:47 +0100

    Improving CSS styles, refactoring and reorganizing sections

--- src/components/organisms/Extensions.tsx
+++ src/components/organisms/Extensions.tsx
@@ -1,9 +1,8 @@
 import { Button } from '../atoms/Button';
 import { browsers } from "../../config/appData";
 
-export const Extensions = () => {
-    return (
-
+export const Extensions= () => {
+return (
         <section id='pricing' className="extension">
             <div className="extension-header">
                 <h2 className="section-header-h2">Download the extension</h2>
@@ -12,16 +11,15 @@ export const Extensions = () => {
                     got a favourite you'd like us to prioritize.
                 </p>
             </div>
-            
-           <div className="extension-cards">
-                {browsers.map((browser, index) => (
-                    <div key={browser.name}
-                        className={`card card-alignment-${index + 1}`}>
-                    <img src={browser.image} alt={browser.name} className="card-img" />
-                    <h3>Add to {browser.name}</h3>
-                    <p>Minimum version {browser.version}</p>
-                    <div className="card-divider"></div> 
-                    <Button variant="blue" className="btn-blue-extension">Add & Install Extension</Button>
+                <div className="extension-cards">
+                {browsers.map((browser) => (
+                    <div key={browser.name} className="card">
+                        <img src={browser.image} alt={browser.name} className="card-img" />
+                        <h3>Add to {browser.name}</h3>
+                        <p>Minimum version {browser.version}</p>
+                        <div className="card-divider"></div>
+                        <Button variant="blue" className="btn-blue-extension">Add & Install Extension</Button>
+
                     </div>
                 ))}
             </div>
