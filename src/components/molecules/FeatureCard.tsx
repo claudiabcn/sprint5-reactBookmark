ef2e726d39a6aa8f8bf8a696d69c426e898ff4a2
CLAUDIA <claudia_4bcn@hotmail.com>
2025-11-27 10:58:46 +0100

    Adding faq section, adding appData information of faqs, adding a new file as molecule with a faq with title and description, adding a new file as organism of faq

--- src/components/molecules/FeatureCard.tsx
+++ src/components/molecules/FeatureCard.tsx
@@ -3,10 +3,10 @@ import { Button } from '../atoms/Button';
 
 const FeatureCard = ({image, title, description}: FeatureCard) => {
     return (
-      <div className="text-center">
-      <img src={image} alt={title} className="mx-auto mb-8 max-h-80" />
-      <h3 className="text-2xl font-semibold mb-4">{title}</h3>
-      <p className="text-gray-600 mb-8 leading-relaxed">{description}</p>
+      <div className="card">
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p> {description}</p>
       <Button variant="blue">
         More Info
       </Button>
