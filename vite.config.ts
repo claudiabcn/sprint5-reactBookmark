1786a2c2be50053b6c636802864919891bbec7e2
Clàudia López Torres <claudia_4bcn@hotmail.com>
2025-12-03 18:18:05 +0100

    Adding config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vitest/config'; 
 import react from '@vitejs/plugin-react';
-import path from 'path'; /
 
 export default defineConfig({
   plugins: [react()],
   
-  // ======================================
-  // 💡 CONFIGURACIÓN DE VITEST
-  // ======================================
   test: {
+    globals: true,
     environment: 'jsdom',
-    globals: true, 
     setupFiles: './src/test/setup.ts', 
+    css: true,
+    
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
   },
 });
 
