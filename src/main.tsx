3595d2750405016a2a70f0c977fb45b5bb24a096
CiberIntegra_05 <CiberIntegra_05@formacio.local>
2025-12-03 10:55:10 +0100

    Separating styles by sections of the landing pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '../styles/_general.css'
+import '../styles/_header.css'
+import '../styles/_hero.css'
+import '../styles/_features.css'
+import '../styles/_extensions.css'
+import '../styles/_faq.css'
+import '../styles/_newsletter.css'
+import '../styles/_footer.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
